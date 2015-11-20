bb037fa2c3bed9efa3e79521ef033e1ad8257d3d
Nathan Lee <nathan@globalphobia.com>
2015-11-20 16:56:52 -0700

    Fix test

--- spec/charlotte_spec.rb
+++ spec/charlotte_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Charlotte do
   it 'has a version number' do
-    expect(Charlotte::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Charlotte::Version).not_to be nil
   end
 end
