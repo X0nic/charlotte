b50c843eab27a206cf35539391279c9d17dca317
Nathan Lee <nathan@globalphobia.com>
2015-12-08 22:48:39 -0700

    Tweak cli commands

--- lib/charlotte/cli.rb
+++ lib/charlotte/cli.rb
@@ -2,34 +2,22 @@ require "thor"
 
 module Charlotte
   class CLI < Thor
-    desc "links DOMAIN", "This will list all links found at a domain"
+    desc "fetch DOMAIN", "This will list all links found at a domain"
     method_option :levels, type: :numeric, default: 99, aliases: "-l"
-    def links(domain)
+    def fetch(domain)
       site_fetcher = SiteFetcher.new(domain, options["levels"])
 
       site_fetcher.fetch
       site_fetcher.print_results
-      site_fetcher.graph_results
     end
 
-    desc "visualize", "Will display visualization of website"
-    def visualize
-      require "graphviz"
+    desc "visualize_pages DOMAIN", "This will visualize all pages found at a domain"
+    method_option :levels, type: :numeric, default: 99, aliases: "-l"
+    def visualize_pages(domain)
+      site_fetcher = SiteFetcher.new(domain, options["levels"])
 
-      # Create a new graph
-      g = GraphViz.new(:G, type: :digraph)
-
-      # Create two nodes
-      hello = g.add_nodes("Hello")
-      world = g.add_nodes("World")
-
-      # Create an edge between the two nodes
-      g.add_edges(hello, world)
-
-      # Generate output image
-      file_name = "hello_world.png"
-      g.output(png: file_name)
-      puts "File saved to: #{file_name}"
+      site_fetcher.fetch
+      site_fetcher.graph_results
     end
 
     desc "version", "Print Charlotte's version information"
