ae0f307dfa1eed35b83edd12aac095e998f17d0a
Nathan Lee <nathan@globalphobia.com>
2015-12-08 23:05:04 -0700

    Don't need to be in constructor

--- lib/charlotte/site_fetcher.rb
+++ lib/charlotte/site_fetcher.rb
@@ -1,22 +1,22 @@
 class SiteFetcher
-  attr_reader :page_registry, :levels, :current_level, :page_fetcher
+  attr_reader :levels, :domain
 
   def initialize(domain, levels)
     @domain = domain
     @levels = levels
-    @current_level = 0
-
-    @page_registry = PageRegistry.new
-    @page_fetcher = PageFetcher.new(domain)
   end
 
   def fetch
+    current_level = 0
+    page_registry = PageRegistry.new
+    page_fetcher = PageFetcher.new(domain)
+
     while current_level < levels
       uris = page_registry.links_to_fetch
       fetch_set = page_fetcher.fetch_set(uris)
       page_registry.uris_fetched(uris)
       page_registry.add_set(fetch_set)
-      @current_level += 1
+      current_level += 1
     end
     page_registry
   end
