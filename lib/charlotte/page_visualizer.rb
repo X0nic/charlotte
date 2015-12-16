8f3712971bd2cb201fda0b9b412a8db271010008
Nathan Lee <nathan@globalphobia.com>
2015-12-16 08:24:37 -0700

    Clean up page_visualizer

--- lib/charlotte/page_visualizer.rb
+++ lib/charlotte/page_visualizer.rb
@@ -6,35 +6,44 @@ class PageVisualizer
   def output
     g = GraphViz.new(:G, type: :digraph, rankdir: "LR")
 
+    nodes = add_graph_nodes(g)
+
+    add_graph_edges(g, nodes)
+
+    generate_output_image(g)
+  end
+
+  private
+
+  def add_graph_nodes(graph)
     print_header("nodes")
     nodes = {}
     page_registry.links.keys.uniq.each do |url|
       puts url unless nodes.key?(url)
-      nodes[url] = g.add_nodes(url) unless nodes.key?(url)
+      nodes[url] = graph.add_nodes(url) unless nodes.key?(url)
     end
 
+    nodes
+  end
+
+  def add_graph_edges(graph, nodes)
     print_header("edges")
     nodes.keys.each do |node_url|
       page = page_registry.page_catalog[node_url]
       next unless page
       page.links.each do |url|
         puts "#{node_url} - #{url}" if nodes.key?(url)
-        g.add_edges(nodes[node_url], nodes[url]) if nodes.key?(url)
+        graph.add_edges(nodes[node_url], nodes[url]) if nodes.key?(url)
       end
     end
+  end
 
-    page_registry.links.each do |link, _status|
-      g.add_nodes(link)
-    end
-
-    # Generate output image
-    file_name = "pages.png"
-    g.output(png: file_name)
+  def generate_output_image(graph)
+    file_name = "assets.png"
+    graph.output(png: file_name)
     puts "File saved to: #{file_name}"
     file_name
   end
-
-  private
 
   attr_reader :page_registry
 
