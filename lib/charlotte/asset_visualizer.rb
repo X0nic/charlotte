779f473390811ec241c03202b762cd95190ea1b7
Nathan Lee <nathan@globalphobia.com>
2015-12-16 08:20:02 -0700

    Clean up asset_visualizer

--- lib/charlotte/asset_visualizer.rb
+++ lib/charlotte/asset_visualizer.rb
@@ -6,35 +6,46 @@ class AssetVisualizer
   def output
     g = GraphViz.new(:G, type: :digraph, rankdir: "LR")
 
+    nodes = add_graph_nodes(g)
+
+    add_graph_edges(g, nodes)
+
+    generate_output_image(g)
+  end
+
+  private
+
+  def add_graph_nodes(graph)
     print_header("nodes")
     nodes = {}
-    page_registry.links_fetched.each do |url|
-      puts url unless nodes.key?(url)
-      nodes[url] = g.add_nodes(url) unless nodes.key?(url)
-    end
-    page_registry.assets.each do |url|
-      puts url unless nodes.key?(url)
-      nodes[url] = g.add_nodes(url) unless nodes.key?(url)
+    [page_registry.links_fetched, page_registry.assets].each do |urls|
+      urls.each do |url|
+        puts url unless nodes.key?(url)
+        nodes[url] = graph.add_nodes(url) unless nodes.key?(url)
+      end
     end
 
+    nodes
+  end
+
+  def add_graph_edges(graph, nodes)
     print_header("edges")
     nodes.keys.each do |node_url|
       page = page_registry.page_catalog[node_url]
       next unless page
       page.assets.each do |url|
         puts "#{node_url} - #{url}" if nodes.key?(url)
-        g.add_edges(nodes[node_url], nodes[url]) if nodes.key?(url)
+        graph.add_edges(nodes[node_url], nodes[url]) if nodes.key?(url)
       end
     end
+  end
 
-    # Generate output image
+  def generate_output_image(graph)
     file_name = "assets.png"
-    g.output(png: file_name)
+    graph.output(png: file_name)
     puts "File saved to: #{file_name}"
     file_name
   end
-
-  private
 
   attr_reader :page_registry
 
